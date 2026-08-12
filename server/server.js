94c6f5c7c31f7d809993f4ac6467ca62ef050f54
Neha Singh <nehasingh03118@gmail.com~>
2026-08-12 10:39:29 +0530

    Fix CORS configuration

--- server/server.js
+++ server/server.js
@@ -9,7 +9,14 @@ const taskRoutes = require("./routes/taskRoutes");
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://localhost:5174"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 app.use(express.json());
 
 // MongoDB connection
